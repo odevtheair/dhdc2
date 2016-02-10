4cd4bd217d95347a6a2811dc3045f853dd22de76
tehnn <tehnnn@gmail.com>
2016-02-11 00:04:20 +0700

    dhdc2

--- frontend/views/portal-qc/index.php
+++ frontend/views/portal-qc/index.php
@@ -4,6 +4,6 @@ use yii\helpers\Html;
 $this->params['breadcrumbs'][] = 'คุณภาพการบันทึก';
 ?>
 <div class="alert alert-success"><p></p></div>
-<p>1)<?=Html::a('EPI คุณภาพ', ['epi-check/index']);?></p>
+<p>1)<?=Html::a('คุณภาพการบันทึกงาน EPI ', ['epi-check/index']);?></p>
 
 
