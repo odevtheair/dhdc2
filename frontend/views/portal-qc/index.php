a35e9bd4ece562e4b051b0cb71760510d4c2e27a
tehnn <tehnnn@gmail.com>
2016-02-16 22:28:22 +0700

    dhdc2

--- frontend/views/portal-qc/index.php
+++ frontend/views/portal-qc/index.php
@@ -7,5 +7,6 @@ $this->params['breadcrumbs'][] = 'คุณภาพการบันทึก'
 <p>1) <?=Html::a('คุณภาพการบันทึกงานสร้างเสริมภูมิคุ้มกันโรค-EPI ', ['epi-check/index']);?></p>
 <p>2) <?=Html::a('คุณภาพการบันทึกงานวางแผนครอบครัว-FP  ', ['fp-check/index']);?></p>
 <p>3) <?=Html::a('คุณภาพการบันทึกงานฝากครรภ์-ANC  ', ['anc-check/index']);?></p>
-<p>4) <?=Html::a('คุณภาพการบันทึกงานคัดกรอง-NCDSCREEN  ', ['epi-check/index']);?></p>
-<p>5) <?=Html::a('คุณภาพการบันทึกงานโภชนาการและพัฒนาการเด็ก-NUTRITION  ', ['epi-check/index']);?></p>
+<p>4) <?=Html::a('คุณภาพการบันทึกงานโภชนาการและพัฒนาการเด็ก-NUTRITION  ', ['nutri-check/index']);?></p>
+<p>5) <?=Html::a('คุณภาพการบันทึกงานคัดกรอง-NCDSCREEN  ', ['screen-check/index']);?></p>
+
